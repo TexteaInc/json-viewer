e5245a51b22c411663aaff8483c149c356d6517d
himself65 <himself65@outlook.com>
2022-09-05 18:03:14 -0500

    refactor: component JsonFunction

--- src/components/DataTypes/Function.tsx
+++ src/components/DataTypes/Function.tsx
@@ -1,13 +1,14 @@
 import React, { useCallback, useState } from 'react'
 
+import type { DataTypeProps } from '../../types/data-type'
 // attribute store for storing collapsed state
 import AttributeStore from './../../stores/ObjectAttributes'
 // theme
 import Theme from './../../themes/getStyle'
 import { DataTypeLabel } from './DataTypeLabel'
 
-export const JsonFunction = (props) => {
-  const [collapsed, setCollapsed] = useState(() => AttributeStore.get(
+export const JsonFunction: React.FC<DataTypeProps<Function>> = (props) => {
+  const [collapsed, setCollapsed] = useState<boolean>(() => AttributeStore.get(
     props.rjvId,
     props.namespace,
     'collapsed',
@@ -22,11 +23,10 @@ export const JsonFunction = (props) => {
       collapsed
     )
   }, [collapsed, props.namespace, props.rjvId])
-  const type_name = 'function'
 
   return (
     <div {...Theme(props.theme, 'function')}>
-      <DataTypeLabel type_name={type_name} {...props} />
+      <DataTypeLabel {...props} type_name='function'/>
       <span
         {...Theme(props.theme, 'function-value')}
         className='rjv-function-container'
@@ -35,17 +35,13 @@ export const JsonFunction = (props) => {
         {collapsed
           ? (
             <span>
-                          {props.value.toString()
-                            .slice(9, -1)
-                            .replace(/\{[\s\S]+/, '')}<span
-                          className='function-collapsed'
-                          style={{ fontWeight: 'bold' }}>
-                             <span>{'{'}</span>
-                             <span {...Theme(props.theme,
-                               'ellipsis')}>...</span>
-                            <span>{'}'}</span>
-                         </span>
-                        </span>
+              {props.value.toString()
+                .slice(9, -1)
+                .replace(/\{[\s\S]+/, '')}<span
+              className='function-collapsed'
+              style={{ fontWeight: 'bold' }}><span>{'{'}</span><span {...Theme(
+                props.theme,
+                'ellipsis')}>...</span><span>{'}'}</span></span></span>
             )
           : props.value.toString().slice(9)}
       </span>
