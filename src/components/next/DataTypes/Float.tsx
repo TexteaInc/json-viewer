b6663880e2ed9f0f658893eb3590503fe674fea8
himself65 <himself65@outlook.com>
2022-09-16 16:39:03 -0500

    fix: remove unused files

--- src/components/next/DataTypes/Float.tsx
+++
@@ -1,9 +0,0 @@
-import type React from 'react'
-
-import type { DataItemProps } from '../../../type'
-
-export const FloatType: React.FC<DataItemProps<number>> = () => {
-  return (
-    <>float</>
-  )
-}
