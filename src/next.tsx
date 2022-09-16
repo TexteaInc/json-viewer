b4df77445f7b6a0007179212af8b00f01772a3ef
himself65 <himself65@outlook.com>
2022-09-16 16:18:19 -0500

    fix: disable user select

--- src/next.tsx
+++ src/next.tsx
@@ -1,33 +1,18 @@
-import type { TreeItemClassKey } from '@mui/lab'
-import type { TreeViewClassKey } from '@mui/lab/TreeView/treeViewClasses'
 import {
   Box,
   createTheme,
   ThemeProvider
 } from '@mui/material'
-import type { OverridesStyleRules } from '@mui/material/styles/overrides'
 import type React from 'react'
 import { useMemo } from 'react'
 
 import { DataKeyPair } from './components/next/DataKeyPair'
 import {
   createJsonViewerStore,
-  DEFAULT_INDENT_WIDTH,
   JsonViewerProvider,
   useJsonViewerStoreApi
 } from './stores/JsonViewerStore'
 import type { ReactJsonViewProps } from './type'
-
-declare module '@mui/material/styles' {
-  export interface Components<Theme = unknown> {
-    MuiTreeView?: {
-      styleOverrides?: Partial<OverridesStyleRules<TreeViewClassKey, 'MuiTreeView', Theme>>
-    }
-    MuiTreeItem?: {
-      styleOverrides?: Partial<OverridesStyleRules<TreeItemClassKey, 'MuiTreeItem', Theme>>
-    }
-  }
-}
 
 const JsonViewerInner: React.FC<ReactJsonViewProps> = (props) => {
   const api = useJsonViewerStoreApi()
@@ -37,7 +22,8 @@ const JsonViewerInner: React.FC<ReactJsonViewProps> = (props) => {
   }))
   return (
     <Box sx={{
-      fontFamily: 'monospace'
+      fontFamily: 'monospace',
+      userSelect: 'none'
     }}>
       <DataKeyPair dataKey='root' value={props.src}/>
     </Box>
@@ -46,17 +32,8 @@ const JsonViewerInner: React.FC<ReactJsonViewProps> = (props) => {
 
 export const JsonViewer: React.FC<ReactJsonViewProps> = (props) => {
   const theme = useMemo(() => createTheme({
-    components: {
-      MuiTreeItem: {
-        styleOverrides: {
-          group: {
-            marginLeft: (props.indentWidth ?? DEFAULT_INDENT_WIDTH) * 4
-          }
-        }
-      }
-    }
     // todo: inject theme based on base16
-  }), [props.indentWidth])
+  }), [])
   return (
     <ThemeProvider theme={theme}>
       <JsonViewerProvider createStore={createJsonViewerStore}>
