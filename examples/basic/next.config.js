c1f6ac906821bc0adc1fbd46f41038218d508e13
himself65 <himself65@outlook.com>
2022-09-22 00:18:14 -0500

    fix(example): add netlify badge

--- examples/basic/next.config.js
+++ examples/basic/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: true,
   images: {
-    domains: ['i.imgur.com']
+    domains: ['i.imgur.com', 'www.netlify.com']
   },
   experimental: {
     externalDir: true
