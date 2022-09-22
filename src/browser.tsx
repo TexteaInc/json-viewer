57a72abc3cd025025bbc820a261ac5a52f9dd6d4
Himself65 <himself65@outlook.com>
2022-09-22 01:15:07 -0500

    feat: support browser (#53)

---
+++ src/browser.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { createRoot, Root } from 'react-dom/client'
+
+import { JsonViewer as JsonViewerComponent } from '.'
+import type { JsonViewerProps } from './type'
+
+const getElementFromConfig = (el?: string | Element) => (el
+  ? (typeof el === 'string' ? document.querySelector(el) : el)
+  : document.getElementById('json-viewer'))
+
+export default class JsonViewer {
+  private props: JsonViewerProps
+  private root?: Root
+
+  static Component = JsonViewerComponent
+
+  constructor (props: JsonViewerProps) {
+    this.props = props
+  }
+
+  render (el?: string | Element) {
+    const container = getElementFromConfig(el)
+
+    if (container) {
+      this.root = createRoot(container)
+      this.root.render(<JsonViewerComponent {...this.props}/>)
+    }
+  }
+
+  destroy () {
+    if (this.root) {
+      this.root.unmount()
+    }
+  }
+}
