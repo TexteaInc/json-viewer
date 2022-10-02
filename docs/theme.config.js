c0e5fef9f3e23a315f25e6600388d5ea5de96e58
himself65 <himself65@outlook.com>
2022-10-02 15:04:52 -0500

    docs: update document

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,5 +1,24 @@
+import { useRouter } from 'next/router'
+
 export default {
+  titleSuffix: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { route } = useRouter()
+    if (route === '/') return ''
+    return ' – Json Viewer'
+  },
+  logo: '@textea/json-viewer',
   project: {
     link: 'https://github.com/TexteaInc/json-viewer'
+  },
+  navigation: {
+    prev: true,
+    next: true
+  },
+  editLink: {
+    text: 'Edit this page on GitHub'
+  },
+  footer: {
+    text: `MIT ${new Date().getFullYear()} © Textea, Inc.`
   }
 }
