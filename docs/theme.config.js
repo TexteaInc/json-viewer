bdae4c5378075ca1ac85f12afcbec69297be7f1a
Pionxzh <spigbbbbb@gmail.com>
2023-01-17 22:53:39 +0800

    docs: remove deprecated `titleSuffix`

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,12 +1,4 @@
-import { useRouter } from 'next/router'
-
 export default {
-  titleSuffix: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { route } = useRouter()
-    if (route === '/') return ''
-    return ' – Json Viewer'
-  },
   logo: '@textea/json-viewer',
   project: {
     link: 'https://github.com/TexteaInc/json-viewer'
