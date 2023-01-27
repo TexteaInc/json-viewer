8cf9039ba03a335e2d96f1651b39e8aa2b25dcb3
Pionxzh <spigbbbbb@gmail.com>
2023-01-27 10:21:48 +0800

    fix: reduce MUI size by using `@swc/plugin-transform-imports` (#169)
    * fix: reduce MUI size by using `@swc/plugin-transform-imports`
    
    * style: apply ESLint rules to enforce correct import path
    
    * fix: fix eslint error message
    
    * fix: add rule for `@mui/material/styles`
    
    * ci: rewrite MUI import differently for esm and cjs
    
    * fix: prevent directory import in ESM build

--- rollup.config.ts
+++ rollup.config.ts
@@ -8,26 +8,21 @@ import replace from '@rollup/plugin-replace'
 import type {
   ModuleFormat,
   OutputOptions,
-  RollupCache,
   RollupOptions
 } from 'rollup'
 import dts from 'rollup-plugin-dts'
 import { defineRollupSwcOption, swc } from 'rollup-plugin-swc3'
 import { fileURLToPath } from 'url'
 
-let cache: RollupCache
-
-const dtsOutput = new Set<[string, string]>()
-
 const outputDir = fileURLToPath(new URL('dist', import.meta.url))
 
-const external = [
+const externalDeps = [
   '@emotion/react',
   '@emotion/styled',
   '@emotion/react/jsx-runtime',
   '@emotion/react/jsx-dev-runtime',
   '@mui/material',
-  '@mui/material/styles',
+  /@mui\/material\/.*/,
   'copy-to-clipboard',
   'zustand',
   'zustand/context',
@@ -37,17 +32,50 @@ const external = [
   'react-dom',
   'react-dom/client'
 ]
+
+const aliasPlugin = alias({
+  entries: [
+    { find: 'react', replacement: '@emotion/react' },
+    { find: 'react/jsx-runtime', replacement: '@emotion/react/jsx-runtime' },
+    { find: 'react/jsx-dev-runtime', replacement: '@emotion/react/jsx-dev-runtime' }
+  ]
+})
+
+const replacePlugin = replace({
+  preventAssignment: true,
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  'typeof window': JSON.stringify('object')
+})
+
+const esmTransformImportsPlugin: [string, Record<string, any>] = [
+  '@swc/plugin-transform-imports',
+  {
+    '@mui/material': { transform: '@mui/material/{{member}}/index.js' },
+    '@mui/material/styles': { transform: '@mui/material/styles/{{member}}.js' }
+  }
+]
+
+const cjsTransformImportsPlugin: [string, Record<string, any>] = [
+  '@swc/plugin-transform-imports',
+  {
+    '@mui/material': { transform: '@mui/material/node/{{member}}' },
+    '@mui/material/styles': { transform: '@mui/material/node/styles/{{member}}' }
+  }
+]
+
 const outputMatrix = (
   name: string, format: ModuleFormat[]): OutputOptions[] => {
   const baseName = basename(name)
-  return format.flatMap(format => ({
+  return format.map(format => ({
     file: resolve(outputDir, `${baseName}.${format === 'es' ? 'm' : ''}js`),
     sourcemap: false,
     name: 'JsonViewer',
     format,
     banner: `/// <reference types="./${baseName}.d.ts" />`,
-    globals: external.reduce((object, module) => {
-      object[module] = module
+    globals: externalDeps.reduce((object, module) => {
+      if (typeof module === 'string') {
+        object[module] = module
+      }
       return object
     }, {} as Record<string, string>)
   }))
@@ -56,84 +84,64 @@ const outputMatrix = (
 const buildMatrix = (input: string, output: string, config: {
   format: ModuleFormat[]
   browser: boolean
-  dts: boolean
-}): RollupOptions => {
-  if (config.dts) {
-    dtsOutput.add([input, output])
-  }
-  return {
-    input,
-    output: outputMatrix(output, config.format),
-    cache,
-    external: config.browser ? [] : external,
-    plugins: [
-      alias({
-        entries: config.browser
-          ? []
-          : [
-              { find: 'react', replacement: '@emotion/react' },
-              {
-                find: 'react/jsx-dev-runtime',
-                replacement: '@emotion/react/jsx-dev-runtime'
-              },
-              {
-                find: 'react/jsx-runtime',
-                replacement: '@emotion/react/jsx-runtime'
+}): RollupOptions[] => {
+  return [
+    ...config.format.map(format => ({
+      input,
+      output: outputMatrix(output, [format]),
+      external: config.browser ? [] : externalDeps,
+
+      plugins: [
+        !config.browser && aliasPlugin,
+        config.browser && replacePlugin,
+        commonjs(),
+        nodeResolve(),
+        swc(defineRollupSwcOption({
+          jsc: {
+            externalHelpers: true,
+            parser: {
+              syntax: 'typescript',
+              tsx: true
+            },
+            transform: {
+              react: {
+                runtime: 'automatic',
+                importSource: '@emotion/react'
               }
-            ]
-      }),
-      config.browser && replace({
-        preventAssignment: true,
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        'typeof window': JSON.stringify('object')
-      }),
-      commonjs(),
-      nodeResolve(),
-      swc(defineRollupSwcOption({
-        jsc: {
-          externalHelpers: true,
-          parser: {
-            syntax: 'typescript',
-            tsx: true
-          },
-          transform: {
-            react: {
-              runtime: 'automatic',
-              importSource: '@emotion/react'
+            },
+            experimental: {
+              plugins: config.browser
+                ? []
+                : format === 'es'
+                  ? [esmTransformImportsPlugin]
+                  : [cjsTransformImportsPlugin]
             }
           }
-        }
-      }))
-    ]
-  }
-}
-
-const dtsMatrix = (): RollupOptions[] => {
-  return [...dtsOutput.values()].flatMap(([input, output]) => ({
-    input,
-    cache,
-    output: {
-      file: resolve(outputDir, `${output}.d.ts`),
-      format: 'es'
-    },
-    plugins: [
-      dts()
-    ]
-  }))
+        }))
+      ]
+    })),
+    {
+      input,
+      output: {
+        file: resolve(outputDir, `${output}.d.ts`),
+        format: 'es'
+      },
+      plugins: [
+        dts()
+      ]
+    }
+  ]
 }
 
 const build: RollupOptions[] = [
-  buildMatrix('./src/index.tsx', 'index', {
-    format: ['es', 'umd'],
-    browser: false,
-    dts: true
+  ...buildMatrix('./src/index.tsx', 'index', {
+    format: ['es', 'cjs'],
+    browser: false
   }),
-  buildMatrix('./src/browser.tsx', 'browser', {
+  ...buildMatrix('./src/browser.tsx', 'browser', {
     format: ['es', 'umd'],
-    browser: true,
-    dts: true
-  }),
-  ...dtsMatrix()
+    browser: true
+  })
 ]
 
 export default build
