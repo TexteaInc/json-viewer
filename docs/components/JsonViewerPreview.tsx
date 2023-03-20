50729b09d69c74f3e3ed7fddfa667e199a4f371c
Pionxzh <spigbbbbb@gmail.com>
2023-03-20 20:57:47 +0800

    docs: improve docs

--- docs/components/JsonViewerPreview.tsx
+++ docs/components/JsonViewerPreview.tsx
@@ -1,11 +1,14 @@
-import type { JsonViewerProps } from '@textea/json-viewer'
+import type { JsonViewerProps, JsonViewerTheme } from '@textea/json-viewer'
 import { JsonViewer } from '@textea/json-viewer'
+import { useTheme } from 'nextra-theme-docs'
 import type { FC } from 'react'
 
 export const JsonViewerPreview: FC<JsonViewerProps> = (props) => {
+  const { theme, systemTheme } = useTheme()
+  const currentTheme = (theme === 'system' ? systemTheme : theme) as JsonViewerTheme
   return (
     <JsonViewer
-      theme='auto'
+      theme={currentTheme}
       style={{
         fontSize: 12
       }}
