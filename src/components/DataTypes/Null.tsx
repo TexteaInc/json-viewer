04478c5528f0412ba4beef2fc64f9842eacd149a
Pionxzh <spigbbbbb@gmail.com>
2023-03-30 01:18:37 +0800

    fix: fix editing on any datatype
    BREAKING CHANGE: dropping `createDataType` and change the signature of EditorComponent to only accept string

--- src/components/DataTypes/Null.tsx
+++ src/components/DataTypes/Null.tsx
@@ -1,32 +1,27 @@
 import { Box } from '@mui/material'
 
 import { useJsonViewerStore } from '../../stores/JsonViewerStore'
-import type { DataType } from '../../type'
 import { createEasyType } from './createEasyType'
 
-export const nullType: DataType<null> = {
+export const nullType = createEasyType<null>({
   is: (value) => value === null,
-  ...createEasyType(
-    'null',
-    () => {
-      const backgroundColor = useJsonViewerStore(store => store.colorspace.base02)
-      return (
-        <Box
-          sx={{
-            fontSize: '0.8rem',
-            backgroundColor,
-            fontWeight: 'bold',
-            borderRadius: '3px',
-            padding: '0.5px 2px'
-          }}
-        >
-          NULL
-        </Box>
-      )
-    },
-    {
-      colorKey: 'base08',
-      displayTypeLabel: false
-    }
-  )
-}
+  type: 'null',
+  colorKey: 'base08',
+  displayTypeLabel: false,
+  Renderer: () => {
+    const backgroundColor = useJsonViewerStore(store => store.colorspace.base02)
+    return (
+      <Box
+        sx={{
+          fontSize: '0.8rem',
+          backgroundColor,
+          fontWeight: 'bold',
+          borderRadius: '3px',
+          padding: '0.5px 2px'
+        }}
+      >
+        NULL
+      </Box>
+    )
+  }
+})
--- src/components/DataTypes/String.tsx
+++ src/components/DataTypes/String.tsx
@@ -2,43 +2,39 @@ import { Box } from '@mui/material'
 import { useState } from 'react'
 
 import { useJsonViewerStore } from '../../stores/JsonViewerStore'
-import type { DataType } from '../../type'
 import { createEasyType } from './createEasyType'
 
-export const stringType: DataType<string> = {
+export const stringType = createEasyType<string>({
   is: (value) => typeof value === 'string',
-  ...createEasyType(
-    'string',
-    (props) => {
-      const [showRest, setShowRest] = useState(false)
-      const collapseStringsAfterLength = useJsonViewerStore(store => store.collapseStringsAfterLength)
-      const value = showRest
-        ? props.value
-        : props.value.slice(0, collapseStringsAfterLength)
-      const hasRest = props.value.length > collapseStringsAfterLength
-      return (
-        <Box
-          component='span'
-          sx={{
-            overflowWrap: 'anywhere',
-            cursor: hasRest ? 'pointer' : 'inherit'
-          }}
-          onClick={() => {
-            if (hasRest) {
-              setShowRest(value => !value)
-            }
-          }}
-        >
-          &quot;
-          {value}
-          {hasRest && !showRest && (<Box component='span' sx={{ padding: 0.5 }}>…</Box>)}
-          &quot;
-        </Box>
-      )
-    },
-    {
-      colorKey: 'base09',
-      fromString: value => value
-    }
-  )
-}
+  type: 'string',
+  colorKey: 'base09',
+  serialize: value => value,
+  deserialize: value => value,
+  Renderer: (props) => {
+    const [showRest, setShowRest] = useState(false)
+    const collapseStringsAfterLength = useJsonViewerStore(store => store.collapseStringsAfterLength)
+    const value = showRest
+      ? props.value
+      : props.value.slice(0, collapseStringsAfterLength)
+    const hasRest = props.value.length > collapseStringsAfterLength
+    return (
+      <Box
+        component='span'
+        sx={{
+          overflowWrap: 'anywhere',
+          cursor: hasRest ? 'pointer' : 'inherit'
+        }}
+        onClick={() => {
+          if (hasRest) {
+            setShowRest(value => !value)
+          }
+        }}
+      >
+        &quot;
+        {value}
+        {hasRest && !showRest && (<Box component='span' sx={{ padding: 0.5 }}>…</Box>)}
+        &quot;
+      </Box>
+    )
+  }
+})
