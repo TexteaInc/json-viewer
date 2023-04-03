8e55eb59cc185ecc1380c8bf25fa757bcf3ac4dd
Pionxzh <spigbbbbb@gmail.com>
2023-04-03 18:53:03 +0800

    docs: add detailed documents for usage and migration

---
+++ docs/examples/JsonViewerPreview.tsx
@@ -0,0 +1,18 @@
+import type { JsonViewerProps } from '@textea/json-viewer'
+import { JsonViewer } from '@textea/json-viewer'
+import type { FC } from 'react'
+
+import { useNextraTheme } from '../hooks/useTheme'
+
+export const JsonViewerPreview: FC<JsonViewerProps> = (props) => {
+  const theme = useNextraTheme()
+  return (
+    <JsonViewer
+      theme={theme}
+      sx={{
+        fontSize: 12
+      }}
+      value={props.value}
+    />
+  )
+}
