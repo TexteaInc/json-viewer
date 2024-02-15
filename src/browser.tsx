922065f67515ea503384f4d0f8596f72e0b8fe93
Pionxzh <pionxzh@csie.io>
2024-02-16 02:13:42 +0800

    feat: expose dataTypes, themes and utils in browser build

--- src/browser.tsx
+++ src/browser.tsx
@@ -2,7 +2,10 @@ import type { Root } from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 
 import { JsonViewer as JsonViewerComponent } from '.'
+import * as dataTypes from './components/DataTypes'
+import * as base16 from './theme/base16'
 import type { JsonViewerProps } from './type'
+import { applyValue, defineDataType, deleteValue, isCycleReference, safeStringify } from './utils'
 
 const getElementFromConfig = (el?: string | Element) => (el
   ? (typeof el === 'string' ? document.querySelector(el) : el)
@@ -13,6 +16,15 @@ export default class JsonViewer {
   private root?: Root
 
   static Component = JsonViewerComponent
+  static DataTypes = dataTypes
+  static Themes = base16
+  static Utils = {
+    applyValue,
+    defineDataType,
+    deleteValue,
+    isCycleReference,
+    safeStringify
+  }
 
   constructor (props: JsonViewerProps) {
     this.props = props
