a7ee0c4e7f7597e38f7b97c219702188125552ed
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-05-18 15:54:36 -0400

    fix: update mdx-components import path

--- docs/src/app/[[...mdxPath]]/page.jsx
+++ docs/src/app/[[...mdxPath]]/page.jsx
@@ -1,6 +1,6 @@
 import { generateStaticParamsFor, importPage } from 'nextra/pages'
 
-import { useMDXComponents as getMDXComponents } from '../../../mdx-components'
+import { useMDXComponents as getMDXComponents } from '../../mdx-components'
 
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
